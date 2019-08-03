21f0d88e32cd85d473d87df7cea4b31231f39da8
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-03 18:24:08 -0400

    defined indexReducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-
+import indexReducer from './reducers/indexReducer';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// set up store
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(indexReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store = {store}>
---
+++ src/reducers/indexReducer.js
@@ -0,0 +1,5 @@
+const indexReducer = (state = {destinations: []}, action) => {
+  return state;
+}
+
+export default indexReducer;
