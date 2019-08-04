7cbf7e146ebbe18c14d10d022b1faf4fbbf9c602
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-04 15:20:07 -0400

    input form is displayed on the page, changed how destination data is displayed with images

--- src/components/DestinationInput.js
+++ src/components/DestinationInput.js
@@ -2,10 +2,56 @@ import React from 'react';
 
 class DestinationInput extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      country: '',
+      image: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const destination = {
+      name: this.state.name,
+      country: this.state.country,
+      image: this.state.image
+    }
+
+    this.props.addDestination(destination);
+
+    this.setState({
+      name: '',
+      country: '',
+      image: ''
+    })
+  }
+
   render() {
     return (
       <div>
-        DestinationInput
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <h3>Add a New Destination</h3>
+
+          <label>Name:</label>
+          <input type='text' name='name' value={this.state.name} onChange={(e) => this.handleChange(e)} /><br/>
+
+          <label>Country:</label>
+          <input type='text' name='country' value={this.state.country} onChange={(e) => this.handleChange(e)} /><br/>
+
+          <label>Image:</label>
+          <input type='text' name='image' value={this.state.image} onChange={(e) => this.handleChange(e)} /><br/><br/>
+
+          <button>Add Destination</button><br/><br/>
+        </form>
       </div>
     )
   }
--- src/components/DestinationsList.js
+++ src/components/DestinationsList.js
@@ -3,7 +3,14 @@ import React from 'react';
 const DestinationsList = props => {
   return (
     <div>
-      {props.destinations.map(destination => <li key={destination.id}>{destination.name} - {destination.country}</li>)}
+      <h3>Destinations</h3>
+      {props.destinations.map(destination =>
+        <li key={destination.id}>
+          {destination.name} - {destination.country}
+          <br/>
+          <img src={destination.image} alt={destination.name} />
+        </li>
+      )}
     </div>
   )
 }
