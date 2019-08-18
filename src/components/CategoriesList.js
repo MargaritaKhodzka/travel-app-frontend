a76d0b081f1c26f730fff6146ae0768fa33d6526
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-18 17:10:37 -0400

    categories section displays the right message

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -5,6 +5,7 @@ const CategoriesList = props => {
 
   return (
     <div className='CategoriesList'>
+      {props.categories && props.categories.length === 0 ? <h6>There are no categories in this location yet</h6> : <h5>Categories in this location</h5>}
       <ListGroup>
         {props.categories && props.categories.map(category =>
           <ListGroupItem key={category.id}>{category.title}</ListGroupItem>
