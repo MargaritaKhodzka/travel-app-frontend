61267d81670832d6a909fc27f1df193c776265a9
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-20 19:37:45 -0400

    created BucketList page

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
-const BucketList = () => {
+const BucketList = props => {
 
   return (
-    <div>
-      Bucket List
+    <div className='DestinationContainer'>
+      <h3>Destinations in your Bucket List</h3>
+
+      {props.destinations.map(destination => {
+        if (destination.bucket_list === true) {
+          return (
+            <Card key={destination.id} className = 'DestinationCard'>
+              <CardBody>
+                <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+              </CardBody>
+            </Card>
+          )
+        } else {
+          return (
+            null
+          )
+        }
+      })}
     </div>
   );
 };
