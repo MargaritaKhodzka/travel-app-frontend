9fb9ed4b51cfc179501ca47f7e51c56925aa579c
MargaritaKhodzka <mkhodzka@gmail.com>
2019-08-21 15:10:56 -0400

    added Links to show from  Visited and Bucket List pages

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 const BucketList = props => {
@@ -10,12 +11,14 @@ const BucketList = props => {
       {props.destinations.map(destination => {
         if (destination.bucket_list === true) {
           return (
-            <Card key={destination.id} className = 'DestinationCard'>
-              <CardBody>
-                <CardTitle>{destination.name} - {destination.country}</CardTitle>
-                <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
-              </CardBody>
-            </Card>
+            <Link key={destination.id} to={`/destinations/${destination.id}`}>
+              <Card key={destination.id} className = 'DestinationCard'>
+                <CardBody>
+                  <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                  <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+                </CardBody>
+              </Card>
+            </Link>
           )
         } else {
           return (
--- src/components/VisitedDestinations.js
+++ src/components/VisitedDestinations.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 
 const VisitedDestinations = props => {
@@ -10,12 +11,14 @@ const VisitedDestinations = props => {
       {props.destinations.map(destination => {
         if (destination.visited === true) {
           return (
-            <Card key={destination.id} className = 'DestinationCard'>
-              <CardBody>
-                <CardTitle>{destination.name} - {destination.country}</CardTitle>
-                <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
-              </CardBody>
-            </Card>
+            <Link key={destination.id} to={`/destinations/${destination.id}`}>
+              <Card className = 'DestinationCard'>
+                <CardBody>
+                  <CardTitle>{destination.name} - {destination.country}</CardTitle>
+                  <CardImg className='DestinationImage' src={destination.image} alt={destination.name} />
+                </CardBody>
+              </Card>
+            </Link>
           )
         } else {
           return (
